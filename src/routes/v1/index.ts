7c0754dbb2e07fe452969d091b5d3119c8cb2af8
levi ackerman <leviackermansnk339@gmail.com>
2026-07-26 00:31:27 -0500

    feat: add auth routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,4 +1,15 @@
 import { Router } from 'express';
+import { validateDTO, authenticate } from '@middlewares';
+
+import { signUpSchema, signInSchema } from '@dtos';
+
+import { signup, signin, logout } from '@controllers';
 
 const router = Router();
+
+// Authentication and authorization endpoints
+router.get('/sign-in', validateDTO(signInSchema), signin);
+router.post('/sign-up', validateDTO(signUpSchema), signup);
+router.post('/logout', authenticate, logout);
+
 export default router;
