2fe162547ba4eed7107b0660b607808aec2bea26
levi ackerman <leviackermansnk339@gmail.com>
2026-07-26 00:30:12 -0500

    feat: add auth controller

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from 'express';
+import { authService } from '@services';
+import { SignUpDto, SignInDto } from '@dtos';
+import { getClientInfo, setAuthCookies, clearAuthCookies } from '@utils';
+
+export async function signup(req: Request, res: Response) {
+  const dto = res.locals.validatedDto as SignUpDto;
+  const { userAgent, ipAddress } = getClientInfo(req);
+  const { user, loginKey, accessToken, refreshToken } = await authService.signup(
+    dto,
+    userAgent,
+    ipAddress
+  );
+  const { username, status } = user;
+
+  setAuthCookies(res, accessToken, refreshToken);
+
+  res.status(201).json({
+    message: 'User created successfully',
+    data: {
+      username,
+      status,
+      loginKey,
+    },
+  });
+}
+
+export async function signin(req: Request, res: Response) {
+  const { userAgent, ipAddress } = getClientInfo(req);
+  const dto = res.locals.validatedDto as SignInDto;
+  const { user, accessToken, refreshToken } = await authService.signin(dto, userAgent, ipAddress);
+  const { username, status } = user;
+
+  setAuthCookies(res, accessToken, refreshToken);
+
+  res.status(201).json({
+    message: 'User logged successfully',
+    data: {
+      username,
+      status,
+    },
+  });
+}
+
+export async function logout(req: Request, res: Response) {
+  const refreshToken: string | undefined = req.cookies?.refreshToken;
+
+  if (refreshToken) {
+    await authService.logout(refreshToken);
+  }
+
+  clearAuthCookies(res);
+
+  res.status(200).json({ message: 'Logged out successfully' });
+}
