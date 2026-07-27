2681d39b78326b25f39dd418e4f1a5c8c3e9903a
levi ackerman <leviackermansnk339@gmail.com>
2026-07-26 22:49:46 -0500

    feat: add messages handler

--- src/websocket/register-event-handlers.ts
+++ src/websocket/register-event-handlers.ts
@@ -1,10 +1,15 @@
 import { createEventDispatcher } from './dispatcher';
+import { WS_CLIENT_EVENTS } from '@constants';
+import { SendMessageWsDto } from '@dtos';
+import { sendMessageHandler } from './handlers';
 
 /**
  * Registers all WebSocket event handlers.
  */
 export function registerEventHandlers() {
   const dispatcher = createEventDispatcher();
+
+  dispatcher.register<SendMessageWsDto>(WS_CLIENT_EVENTS.SEND_MESSAGE, sendMessageHandler);
 
   return dispatcher;
 }
