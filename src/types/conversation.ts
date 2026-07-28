8ffee4efe0c859a70e589453c9a9269c445a9401
levi ackerman <leviackermansnk339@gmail.com>
2026-07-28 16:52:16 -0500

    feat: add messaging types

---
+++ src/types/conversation.ts
@@ -0,0 +1,15 @@
+export enum MessageStatus {
+  SENT = 'SENT',
+  DELIVERED = 'DELIVERED',
+  READ = 'READ',
+}
+
+export enum ConversationRole {
+  OWNER = 'OWNER',
+  MEMBER = 'MEMBER',
+}
+
+export enum ConversationType {
+  PRIVATE = 'PRIVATE',
+  GROUP = 'GROUP',
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from './user';
 export * from './websocket';
+export * from './conversation';
