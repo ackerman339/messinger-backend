d17b4435e9682eaa81994bba59a4d012071f5310
levi ackerman <leviackermansnk339@gmail.com>
2026-07-31 11:45:30 -0500

    feat: add group conversations types

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,6 +1,9 @@
+import { UserRole } from '@appTypes';
 declare global {
   namespace Express {
     interface Locals {
+      userId?: string;
+      roles?: UserRole[];
       validatedDto: Record<string, unknown>;
     }
   }
