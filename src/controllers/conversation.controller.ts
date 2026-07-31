01afcafed419839c30564ebfb8df53f6102e9450
levi ackerman <leviackermansnk339@gmail.com>
2026-07-31 11:51:12 -0500

    feat: add group conversations endpoints

---
+++ src/controllers/conversation.controller.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from 'express';
+import { conversationService, messageService } from '@services';
+import {
+  CreateGroupDto,
+  LeaveGroupDto,
+  TransferOwnershipDto,
+  RemoveMemberDto,
+  DeleteConversationDto,
+  GetConversationMessagesDto,
+} from '@dtos';
+
+export async function getUserConversations(_req: Request, res: Response) {
+  const userId = res.locals.userId!;
+  const conversations = await conversationService.getUserConversations(userId);
+
+  res.status(200).json({
+    message: 'Conversations loaded',
+    data: conversations,
+  });
+}
+
+export async function getConversationMessages(_req: Request, res: Response) {
+  const dto = res.locals.validatedDto as GetConversationMessagesDto;
+  const userId = res.locals.userId!;
+  const conversations = await messageService.getConversationMessages(userId, dto.conversationId);
+
+  res.status(200).json({
+    message: 'Conversations loaded',
+    data: conversations,
+  });
+}
+
+export async function createGroup(_req: Request, res: Response) {
+  const dto = res.locals.validatedDto as CreateGroupDto;
+  const userId = res.locals.userId!;
+  const group = await conversationService.createGroup(userId, dto);
+
+  res.status(201).json({
+    message: 'Group created successfully',
+    data: {
+      id: group.id,
+      name: group.name,
+    },
+  });
+}
+
+export async function leaveGroup(_req: Request, res: Response) {
+  const dto = res.locals.validatedDto as LeaveGroupDto;
+  const userId = res.locals.userId!;
+  await conversationService.leaveGroup(userId, dto.conversationId);
+
+  res.sendStatus(204);
+}
+
+export async function transferGroupOwnership(_req: Request, res: Response) {
+  const dto = res.locals.validatedDto as TransferOwnershipDto;
+  const userId = res.locals.userId!;
+  await conversationService.transferGroupOwnership(userId, dto);
+
+  res.sendStatus(204);
+}
+
+export async function removeGroupMember(_req: Request, res: Response) {
+  const dto = res.locals.validatedDto as RemoveMemberDto;
+  const userId = res.locals.userId!;
+  await conversationService.removeGroupMember(userId, dto);
+
+  res.sendStatus(204);
+}
+
+export async function deleteGroup(_req: Request, res: Response) {
+  const dto = res.locals.validatedDto as DeleteConversationDto;
+  const userId = res.locals.userId!;
+  await conversationService.deleteGroup(userId, dto);
+
+  res.sendStatus(204);
+}
+
+export async function deletePrivateConversation(_req: Request, res: Response) {
+  const dto = res.locals.validatedDto as DeleteConversationDto;
+  const userId = res.locals.userId!;
+  await conversationService.deletePrivateConversation(userId, dto.conversationId);
+
+  res.sendStatus(204);
+}
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,30 @@
 import { Router } from 'express';
 import { validateDTO, authenticate } from '@middlewares';
 
-import { signUpSchema, signInSchema } from '@dtos';
+import {
+  signUpSchema,
+  signInSchema,
+  CreateGroupSchema,
+  LeaveGroupSchema,
+  TransferOwnershipSchema,
+  DeleteConversationSchema,
+  GetConversationMessagesSchema,
+  RemoveMemberSchema,
+} from '@dtos';
 
-import { signup, signin, logout } from '@controllers';
+import {
+  signup,
+  signin,
+  logout,
+  createGroup,
+  leaveGroup,
+  transferGroupOwnership,
+  deleteGroup,
+  deletePrivateConversation,
+  getUserConversations,
+  getConversationMessages,
+  removeGroupMember,
+} from '@controllers';
 
 const router = Router();
 
@@ -11,5 +32,50 @@ const router = Router();
 router.get('/sign-in', validateDTO(signInSchema), signin);
 router.post('/sign-up', validateDTO(signUpSchema), signup);
 router.post('/logout', authenticate, logout);
+
+// Conversation endpoints
+router.get('/conversation', authenticate, getUserConversations);
+router.post('/conversation/leave-group', authenticate, validateDTO(LeaveGroupSchema), leaveGroup);
+router.get(
+  '/conversation/messages',
+  authenticate,
+  validateDTO(GetConversationMessagesSchema),
+  getConversationMessages
+);
+
+router.post(
+  '/conversation/create-group',
+  authenticate,
+  validateDTO(CreateGroupSchema),
+  createGroup
+);
+
+router.post(
+  '/conversation/transfer-ownership',
+  authenticate,
+  validateDTO(TransferOwnershipSchema),
+  transferGroupOwnership
+);
+
+router.post(
+  '/conversation/remove-group-member',
+  authenticate,
+  validateDTO(RemoveMemberSchema),
+  removeGroupMember
+);
+
+router.delete(
+  '/conversation/delete-group',
+  authenticate,
+  validateDTO(DeleteConversationSchema),
+  deleteGroup
+);
+
+router.delete(
+  '/conversation/delete-private',
+  authenticate,
+  validateDTO(DeleteConversationSchema),
+  deletePrivateConversation
+);
 
 export default router;
