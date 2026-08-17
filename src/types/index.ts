e59086a5250d9472cdbf59ffc1244dba7702e772
levi ackerman <leviackermansnk339@gmail.com>
2026-08-17 17:08:52 -0500

    feat: add webpush types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './user';
 export * from './websocket';
 export * from './conversation';
+export * from './push-subscription';
---
+++ src/types/push-subscription.ts
@@ -0,0 +1,11 @@
+export enum PushPlatform {
+  WEB = 'WEB',
+  ANDROID = 'ANDROID',
+  IOS = 'IOS',
+}
+
+export enum PushProvider {
+  WEB_PUSH = 'WEB_PUSH',
+  FCM = 'FCM',
+  APNS = 'APNS',
+}
